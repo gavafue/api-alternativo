8e8dc89c6d51cd7ecb23b4e0944a00a5b4632726
gavafue <88394722+gavafue@users.noreply.github.com>
2021-09-10 09:57:49 -0300

    Update TeamLists.js

--- js/TeamLists.js
+++ js/TeamLists.js
@@ -1,21 +1,21 @@
-var showTeam = function(array,lugar){
-    for (let equipo in array){
-        contenido="";
+var showTeam = function (array, lugar) {
+    contenido = "";
+    for (let equipo in array) {
         contenido += 'Equipo: ' + equipo.namecomplete + '<br>';
-                    contenido += 'Jugadores: ' + equipo.jugadores + '<br>';
-                    contenido += 'Habilitados: ' + equipo.habilitados + '<br>';
-                    contenido += 'Sanciones: ' + equipo.sanciones + '<br>';
-                    contenido += 'Diferencia de Goles: ' + equipo.diferenciaGoles + '<br>';
-                    contenido += `<img src="${equipo.img}" width="250px"></img>"`;
-                    contenido += '<br><hr><br>'
+        contenido += 'Jugadores: ' + equipo.jugadores + '<br>';
+        contenido += 'Habilitados: ' + equipo.habilitados + '<br>';
+        contenido += 'Sanciones: ' + equipo.sanciones + '<br>';
+        contenido += 'Diferencia de Goles: ' + equipo.diferenciaGoles + '<br>';
+        contenido += `<img src="${equipo.img}" width="250px"></img>"`;
+        contenido += '<br><hr><br>'
     }
-    document.getElementById(lugar).innerHTML=contenido;
+    document.getElementById(lugar).innerHTML = contenido;
 }
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     getJSONData(MAYORES_FEMENINO).then(function (resultObj) {
         if (resultObj.status === "ok") {
-                  showTeam(resultObj.data,"mayoresFemenino");
+            showTeam(resultObj.data, "mayoresFemenino");
         }
     });
 })
