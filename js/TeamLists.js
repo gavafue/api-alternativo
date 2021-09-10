88845bf5ae8efe2e9396eaaedd6ce2824b735a25
gavafue <88394722+gavafue@users.noreply.github.com>
2021-09-10 12:27:34 -0300

    Update TeamLists.js

--- js/TeamLists.js
+++ js/TeamLists.js
@@ -1,27 +1,31 @@
 var showTeam = function (array, lugar) {
     contenido = "";
-    array.forEach(element => {
-        contenido += 
-        `<div class="teamcontainer"><button class="submit horizontal">Más información</button><img src="${element.img}" class="teamlogo"></img>
+    for (let i = 0; i < array.length; i++) {
+        let element=array[i];
+        contenido +=
+            `<div class="teamcontainer"><button class="submit horizontal">Más información</button><img src="${element.img}" class="teamlogo"></img>
         <p><i>Equipo:</i> ${element.namecomplete}</p>
         <p><i>Jugadores:</i> ${element.jugadores}</p>
         <p><i>Habilitados:</i> ${element.habilitados}</p>
         <p><i>Sanciones:</i> ${element.sanciones}</p>
         <p><i>Diferencia de Goles:</i> ${element.diferenciaGoles}</p>
     </div><hr>`
-    });
-       document.getElementById(lugar).innerHTML = contenido;
+    };
+document.getElementById(lugar).innerHTML = contenido;
 }
-var arrayMayoresFem 
+var arrayMayoresFem
 document.addEventListener("DOMContentLoaded", function () {
     getJSONData(MAYORES_FEMENINO).then(function (resultObj) {
         if (resultObj.status === "ok") {
             showTeam(resultObj.data, "mayoresFemenino");
-            arrayMayoresFem=resultObj.data
+            arrayMayoresFem = resultObj.data
         }
     });
 
-    document.getElementsByClassName("horizontal").addEventListener("click",function(){
-window.location= "info-detallada.html";
-    });
+    function moreteaminfo(n) {
+
+        localStorage.setItem("team", n)
+        window.location = "info-detallada.html"
+    }
+
 })
