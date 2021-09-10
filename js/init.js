e1797ee6f63b5314be5a7f5975c3917024b8690b
gavafue <88394722+gavafue@users.noreply.github.com>
2021-09-10 09:23:19 -0300

    Cambios

---
+++ js/init.js
@@ -0,0 +1,29 @@
+MAYORES_FEMENINO = "https://gavafue.github.io/api-alternativo/json/mayoresfem.json"
+
+var getJSONData = function (url) {
+    var result = {};
+    return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .then(function (response) {
+        result.status = 'ok';
+        result.data = response;
+        return result;
+      })
+      .catch(function (error) {
+        result.status = 'error';
+        result.data = error;
+        return result;
+      });
+  }
+
+  
+document.addEventListener("DOMContentLoaded",function(){
+   
+
+})
