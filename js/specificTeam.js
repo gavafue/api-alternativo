03057678363e2e95ab9aa765d726d2608e8553da
gavafue <88394722+gavafue@users.noreply.github.com>
2021-10-06 22:50:47 -0300

    json

--- js/specificTeam.js
+++ js/specificTeam.js
@@ -48,7 +48,24 @@ document.addEventListener("DOMContentLoaded", function () {
             result.data.forEach(element => {
                 var buttonWD
                 if (element.writedreport=="yes") {
-                    buttonWD="hola";
+                    buttonWD=`<span class="badge bg-warning text-dark" style="cursor:pointer;" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver WD</span>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Informe escrito: ${element.name}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>`;
                     
                 }
                 else{
