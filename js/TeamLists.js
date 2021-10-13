332949ba16fb382c060cd084eb01752d5e927a3c
gavafue <88394722+gavafue@users.noreply.github.com>
2021-10-13 13:40:51 -0300

    fggdf

--- js/TeamLists.js
+++ js/TeamLists.js
@@ -3,27 +3,38 @@ var showTeam = function (array, lugar) {
     for (let i = 0; i < array.length; i++) {
         let element=array[i];
         contenido +=
-            `  <div class="row bg-light border" style="cursor:pointer;" onclick="moreteaminfo(${element.id})">
-            <div class="col-2 img-hover-zoom img-hover-zoom--brightness"><img src="${element.img}"
-                    class="img-thumbnail container-fluid" style="max-height: 8rem;"></img></div>
-            <div class="col">
-                <div class="row " style="text-align:center;"><b>${element.namecomplete}</b></div>
-                <div class="row ">
-                    <div class="col-3 ">Jugadores:</div>
-                    <div class="col"> ${element.jugadores}</div>
+            ` <div class="col-4"> <div class="card mb-3" style="max-width: 540px; cursor:pointer;" onclick="moreteaminfo(${element.id})">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="${element.img}" class="img-fluid rounded-start" alt="logo del equipo ${element.namecomplete}">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center" style="font-size:16px">${element.namecomplete}</h5>
+                  <p class="card-text">
+                    <div class="row ">
+                      <div class="col-8">Jugadores:</div>
+                      <div class="col text-center"> ${element.jugadores}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-8">Habilitados:</div>
+                      <div class="col text-center"> ${element.habilitados}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-8">Sanciones:</div>
+                      <div class="col text-center">${element.sanciones}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-8">Diferencia de Goles:</div>
+                      <div class="col text-center"> ${element.diferenciaGoles}</div>
+                    </div>
+                  </p>
+                  
                 </div>
-                <div class="row">
-                    <div class="col-3">Habilitados:</div>
-                    <div class="col"> ${element.habilitados}</div>
-                </div>
-                <div class="row">
-                   <div class="col-3">Sanciones:</div><div class="col">${element.sanciones}</div>
-                </div>
-                <div class="row">
-                   <div class="col-3">Diferencia de Goles:</div><div class="col"> ${element.diferenciaGoles}</div>
-                </div>
+              </div>
             </div>
-        </div>`
+          </div>
+          </div>`
     };
 document.getElementById(lugar).innerHTML = contenido;
 }
