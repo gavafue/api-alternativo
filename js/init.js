e123033da308b950ba8e3a6a304b0e24c2868170
gavafue <88394722+gavafue@users.noreply.github.com>
2021-10-14 13:58:57 -0300

    log in y validaciones

--- js/init.js
+++ js/init.js
@@ -1,6 +1,26 @@
 MAYORES_FEMENINO = "https://gavafue.github.io/api-alternativo/json/mayoresfem.json"
 PONTEVEDRES_MAY_FEM = "https://gavafue.github.io/api-alternativo/json/pontevedresmayoresfem.json"
 INFORMES_MAY_FEM = "https://gavafue.github.io/api-alternativo/json/informesfemMay.json"
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  let userLogged = localStorage.getItem('User-Logged');
+  let infoUser = document.getElementById("info-user")
+  let user = document.getElementById("user");
+
+  if (userLogged) {
+    userLogged = JSON.parse(userLogged);
+    user.innerText = user.innerText + 'Usuario logueado: ' + userLogged.email;
+    infoUser.style = "display: inline-block";
+  }
+
+  if (document.getElementById("salir")) {
+    document.getElementById("salir").addEventListener("click", function () {
+      localStorage.removeItem('User-Logged');
+      window.location = 'login.html';
+    })
+  }
+});
+
 var getJSONData = function (url) {
     var result = {};
     return fetch(url)
--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
     document.getElementById("submitBtn").addEventListener("click", function (e) {
-        window.location = 'indice.html';
+        let inputEmail = document.getElementById("form3Example3");
+        let inputPassword = document.getElementById("form3Example4");
+        let camposCompletos = true;
+
+        if (inputEmail.value === '') {
+            inputEmail.classList.add("bg-danger");
+            camposCompletos = false;
+        }
+
+        if (inputPassword.value === '') {
+            inputPassword.classList.add("bg-danger");
+            camposCompletos = false;
+        }
+
+        if (camposCompletos) {
+
+            localStorage.setItem('User-Logged', JSON.stringify({ email: inputEmail.value }));
+
+            window.location = 'index.html';
+
+        } else {
+            alert("Debes ingresar los datos!")
+        }
     });
 });
